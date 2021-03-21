ba3cfb1c81443b57a127fc7bfc742fb84623fba7
Neestackich <neestackich@gmail.com>
2021-03-21 15:21:42 +0300

    Resolved 6-8 tasks

--- lab01/lfsr.cpp
+++ lab01/lfsr.cpp
@@ -2,6 +2,7 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    unsigned b = ((*reg >> 2) ^ (*reg >> 3) ^ (*reg >> 5) ^ *reg) & 1;
+    *reg >>= 1;
+    *reg = (*reg & ~ (1 << 15)) | (b << 15);
 }
-
